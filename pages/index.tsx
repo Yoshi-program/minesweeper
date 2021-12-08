ac2cb6058c5891a867d8654253925ccc4ee7e32d
s1F102103303 <s1F102103303@iniad.org>
2021-12-08 18:42:54 +0900

    細かい修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,25 @@ import type { NextPage } from 'next'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  background-color: blue;
   height: 1000px;
+  background-color: blue;
 `
 const Board = styled.div`
-  background-color: white;
   position: absolute;
-  left: 50%;
   top: 50%;
-  margin-left: -100px;
-  margin-top: -100px;
+  left: 50%;
   width: 200px;
   height: 200px;
+  margin-top: -100px;
+  margin-left: -100px;
+  background-color: white;
 `
 const Face = styled.div`
-  width: 50px; /*幅*/
-  height: 50px; /*高さ*/
-  border-radius: 50%; /*角丸*/
-  background-color: #ffcd8c;
+  width: 50px; /* 幅 */
+  height: 50px; /* 高さ */
   margin: auto;
+  background-color: #ffcd8c;
+  border-radius: 50%; /* 角丸 */
 `
 const BlockArea = styled.div`
   width: 144px;
@@ -29,12 +29,12 @@ const BlockArea = styled.div`
   background-color: gray;
 `
 const Block = styled.div`
+  float: left;
   width: 16px;
   height: 16px;
-  border: solid 1px;
-  float: left;
-  background-color: #cccccc;
   vertical-align: baseline;
+  background-color: #ccc;
+  border: solid 1px;
 `
 
 const Home: NextPage = () => {
