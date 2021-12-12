36c7beeb18dad17fb2295597278bc4b73c8a9a1c
s1F102103303 <s1F102103303@iniad.org>
2021-12-12 10:56:31 +0900

    サイズ調整と目を作ってみた

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,62 @@ import type { NextPage } from 'next'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  height: 1000px;
+  height: 800px;
   background-color: blue;
 `
 const Board = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  width: 200px;
-  height: 200px;
-  margin-top: -100px;
-  margin-left: -100px;
+  width: 400px;
+  height: 400px;
+  margin: 0;
+  margin-right: -50%;
+  background: yellow;
   background-color: white;
+  transform: translate(-50%, -50%);
 `
 const Face = styled.div`
+  position: relative;
+  top: 15px;
   width: 50px; /* 幅 */
   height: 50px; /* 高さ */
   margin: auto;
   background-color: #ffcd8c;
+  border: solid 2px;
   border-radius: 50%; /* 角丸 */
 `
+const RightEyes = styled.div`
+  position: relative;
+  top: 13px;
+  left: 9px;
+  width: 11px;
+  height: 11px;
+  background-color: black;
+  border-radius: 50%;
+`
+const LeftEyes = styled.div`
+  position: relative;
+  top: 2px;
+  left: 26px;
+  width: 11px;
+  height: 11px;
+  background-color: black;
+  border-radius: 50%;
+`
+
 const BlockArea = styled.div`
-  width: 144px;
-  height: 144px;
+  position: relative;
+  top: 30px;
+  width: 306px;
+  height: 306px;
   margin: auto;
   background-color: gray;
 `
 const Block = styled.div`
   float: left;
-  width: 16px;
-  height: 16px;
+  width: 34px;
+  height: 34px;
   vertical-align: baseline;
   background-color: #ccc;
   border: solid 1px;
@@ -41,7 +67,10 @@ const Home: NextPage = () => {
   return (
     <Container>
       <Board>
-        <Face></Face>
+        <Face>
+          <RightEyes></RightEyes>
+          <LeftEyes></LeftEyes>
+        </Face>
         <BlockArea>
           <Block></Block>
           <Block></Block>
