149a08cc1d377bbd3274173f79069ec9a0bc9516
s1F102103303 <s1F102103303@users.noreply.github.com>
2021-12-13 10:51:50 +0000

    deploy: 3fe8840225b34cd492f12f3da68f1508d2e01b30

---
+++ _next/static/qmXUd2iYjI5v2g_toX071/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/163-c98103972be1d7024d11.js","static/chunks/pages/index-62686ac915b36da23c89.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
